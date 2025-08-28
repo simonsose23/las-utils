9ff43f06ac4e5087e18e2d66d1872cc2f57aea63
simon <si@m.on>
2025-08-28 15:19:25 +0200

    Test rename

--- tests/test_scalar_processor.py
+++ tests/test_scalar_processor.py
@@ -3,13 +3,13 @@ import src.pc_class_processor as pc_proc
 
 class MockLasObject():
 
-    def __init__(self, scalar_array, scalar_field='mock_field_name'):
+    def __init__(self, scalar_array, scalar_field='mock_field_name', empty_col_name='foo'):
         self.remove_extra_dim_calls = []
 
-        self.points = np.array(scalar_array, dtype=[(scalar_field, np.int8)])
+        self.points = np.array(scalar_array, dtype=[(scalar_field, np.int8), (empty_col_name, np.int8)])
 
     def remove_extra_dim(self, scalar_field):
-        self.num_remove_extra_dim_calls.append(scalar_field)
+        self.remove_extra_dim_calls.append(scalar_field)
 
 def test_cluster_classes_clustering():
     # configure las object
@@ -24,3 +24,13 @@ def test_cluster_classes_clustering():
 
     assert np.array_equal(las_object.points[scalar_field], np.array([1, 4, 4, 6, 6]))
     assert len(las_object.remove_extra_dim_calls) == 0
+
+def test_cluster_classes_rename():
+    # configure las object
+    scalar_array = [1]
+    las_object = MockLasObject(scalar_array)
+
+    pc_proc.cluster_classes(las_object, {}, 'mock_field_name', 'foo')
+
+    assert las_object.remove_extra_dim_calls == ['mock_field_name']
+    assert np.array_equal(las_object.points['foo'], np.array([1]))
